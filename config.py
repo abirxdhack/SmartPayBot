3a3bda5d1c04d3978b5cfc06cef796bed0727599
Abir Arafat Chawdhury <abirxdhackz.info.me@gmail.com>
2025-04-18 21:15:14 +0600

    Added ADMIN_IDS

--- config.py
+++ config.py
@@ -2,3 +2,6 @@ API_ID = "YOUR API ID HERE"
 API_HASH = "YOUR API HASH HERE"
 BOT_TOKEN = "YOUR BOT TOKEN HERE"
 COMMAND_PREFIX = [",", ".", "/", "!"]
+
+# Add your admin user IDs here
+ADMIN_IDS = [123456789, 987654321]  # Replace with actual Telegram user IDs
